0c45ecf3dcb264ec5d9efbf110cfd0acaf02e7a8
bugbear <yuhe.wei@upai.com>
2018-01-11 15:09:57 +0800

    something just like this

--- example/.sling.js
+++ example/.sling.js
@@ -8,7 +8,7 @@ module.exports = {
   scripts: ['npm run test'],
   slack: {
     project: 'test',
-    webhook: 'https://hooks.slack.com/services/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',
+    webhook: 'https://hooks.slack.com/services/T2AV0RV8E/B2UQU57HU/*****',
     channel: 'general',
     username: 'bug-dog'
   },
